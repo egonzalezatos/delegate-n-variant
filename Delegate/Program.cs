586187a7a492e3a1957b370e8ed762181c07c76e
kurayamigin <edwin-julian.gonzalez@atos.net>
2022-03-23 13:07:06 -0600

    first commit

---
+++ Delegate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Delegate
+{
+    class Program
+    {
+        //signature  <output> Nombre(<input> s);
+        delegate string Delegado1(string s);
+        delegate T DelegadoGenerico<T>(string s);
+        
+        static void Main(string[] args)
+        {
+            Delegado1 n = Imprimir;
+            Delegado1 n2 = n;
+            Delegado1 n3 = n2;
+            
+            //Imprimir("hola mundo");
+            n2("hola mundo");
+        }
+        
+        static string Imprimir(string s)  //Signature:  <output> Imprimir(<input> s);
+        {
+            Console.WriteLine(s);
+            return s;
+        }
+    }
+}
---
+++ Variant/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Delegate_n_Variant
+{
+    class Program
+    {
+        class Animal {}       // Base
+        class Dog : Animal {} // Child
+        
+        interface ICovarianza<out T>{}          //      out -> covarianza
+        interface IContravarianza<in T> { }     //      in -> Contravarianza
+        interface IInvarianza<T> {}             // default -> Invarianza
+        //Variant : Co-Contra Variance
+        class Variant<T> : ICovarianza<T>, IContravarianza<T>, IInvarianza<T>
+        {}
+        
+        static void Main(string[] args)
+        {
+            ICovarianza<Animal>  c1 = new Variant<Dog>();       // Animal <- Dog
+            IContravarianza<Dog> c2 = new Variant<Animal>();    // Dog    <- Animal 
+            IInvarianza<Animal>  c3 = new Variant<Animal>();
+        }
+    }
+}
